983582bb8928b72689c74a16bab9088e8179dcbe
brianzakariyahusein <brianzakariyahusein@gmail.com>
2025-02-20 10:57:18 +0700

    Menambahkan fitur register dan hasing passwor dengan bcrypt

--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -1,9 +1,37 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
+const User = require("../models/userModel");
 const router = express.Router();
 
 // Endpoint untuk regiter
-router.post("/register", (req, res) => {
-  res.send("Register endpoint");
+router.post("/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // Cek user sudah ada atau belum
+    const userExist = await User.findOne({ email });
+    if (userExist) {
+      return res.status(400).json({ message: "Email sudah digunakan" });
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Buat user baru
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    // Simpan user ke database
+    await user.save();
+
+    res.status(201).json({ message: "User berhasil didaftarkan" });
+  } catch (error) {
+    res.status(500).json({ message: "Terjadi kesalahan server..." });
+  }
 });
 
 // Endpoint untuk login
