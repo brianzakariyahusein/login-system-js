61f25c6c7410e547011076a94cb98e285e820ccd
brianzakariyahusein <brianzakariyahusein@gmail.com>
2025-02-22 16:38:57 +0700

    Refactor code update

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }, // Harus hash, bukan plain text
+  password: { type: String, required: true },
 });
 
 module.exports = mongoose.model("User", UserSchema);
